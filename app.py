52ee1f1ab77b8e9e4b81869b4c61a40cba64928b
ryann4real_ <ryanmeira099@gmail.com>
2026-05-05 09:19:53 -0300

    Update app.py

--- app.py
+++ app.py
@@ -12,15 +12,12 @@ if st.button("Calcular"):
     st.success(f"Seu imc é {imc: .2f}")
 
     if imc < 18.5:
-        st.image("https://i.ytimg.com/vi/ydAjWzeSz0s/maxresdefault.jpg")
         st.error("Abaixo do peso ☠️")
 
     if imc <= 24.9:
-        st.image("https://tse1.explicit.bing.net/th/id/OIP.6gB_bdjJ7GygfvaRGIFAIAHaHa?rs=1&pid=ImgDetMain&o=7&rm=3")
         st.info("Seu peso esta perfeito!")
 
     if imc >= 29.9:
-        st.image("https://tse1.mm.bing.net/th/id/OIP.Z47e1SbM_WltJEbb_oEAWQAAAA?rs=1&pid=ImgDetMain&o=7&rm=3")
         st.warning("Acima do peso")  
               
     if imc >= 34.9:
